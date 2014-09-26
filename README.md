f0e240795ee8b6cc8cd8692227da3836e10cc131
H. İbrahim Güngör <igungor@gmail.com>
2014-09-26 16:52:34 +0300

    initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# s3del
+
+[Wercker] step to delete a file from [Amazon S3].
+
+You can use application and deployment variables in wercker.
+
+View this step in the [wercker directory][wd]
+
+## Options
+
+* `key-id` (required) The Amazon Access key that will be used for authorization.
+* `key-secret` (required) The Amazon Access secret that will be used for authorization.
+* `url` (required) The S3 URL denoting the file's destination.
+
+## Example
+
+    - s3del:
+        key-id:     $KEY
+        key-secret: $SECRET
+        url:        s3://mybucket/myproject-latest.tar.gz
+
+[Wercker]: http://wercker.com
+[Amazon S3]: http://aws.amazon.com/s3
---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+set -e
+cd $HOME
+if [ ! -n "$WERCKER_S3DEL_KEY_ID" ]
+then
+    fail 'missing or empty option key_id, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3DEL_KEY_SECRET" ]
+then
+    fail 'missing or empty option key_secret, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3DEL_URL" ]
+then
+    fail 'missing or empty option url, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3DEL_RECURSIVE" ]
+then
+    RECURSIVE=""
+else
+    RECURSIVE="--recursive"
+fi
+
+if ! type s3cmd &> /dev/null ;
+then
+    info 's3cmd not found, start installing it'
+    wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+    sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+    sudo apt-get update && sudo apt-get install s3cmd
+    success 's3cmd installed succesfully'
+else
+    info 'skip s3cmd install, command already available'
+    debug "type s3cmd: $(type s3cmd)"
+fi
+
+if [ -e '.s3cfg' ]
+then
+    warn '.s3cfg file already exists in home directory and will be overwritten'
+fi
+
+echo '[default]' > '.s3cfg'
+echo "access_key=$WERCKER_S3DEL_KEY_ID" >> .s3cfg
+echo "secret_key=$WERCKER_S3DEL_KEY_SECRET" >> .s3cfg
+debug "generated .s3cfg for key $WERCKER_S3DEL_KEY_ID"
+
+info 'starting s3 delete'
+
+# TODO make public an option
+
+cd "$WERCKER_ROOT"
+
+set +e
+debug "s3cmd del --verbose $RECURSIVE '$WERCKER_S3DEL_URL'"
+sync_output=$(s3cmd sync --verbose "$WERCKER_S3DEL_URL")
+
+if [[ $? -ne 0 ]];then
+    warning $sync_output
+    fail 's3cmd failed';
+else
+    success 'finished s3 delete';
+fi
+set -e
